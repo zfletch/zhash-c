e50961a27334af25214802c7393536f8ff049dca
zfletch <zfletch2@gmail.com>
2023-05-06 10:37:10 -0400

    use leaks command for macos
    On macOS, instead of using Valgrind, use the `leaks` command.
    Valgrind doesn't work on M1/M2 machines.

--- test/zhash_test.sh
+++ test/zhash_test.sh
@@ -2,6 +2,33 @@
 
 gcc -Wall -Wextra -g ../src/zhash.c ./zhash_test.c -o hash
 
-./hash && valgrind --quiet --leak-check=yes ./hash
+./hash
+if [ $? -ne 0 ]; then
+  echo '[FAIL]: At least one test failed'
+  exit 1
+fi
+
+# check for memory leaks on Linux
+if command -v valgrind > /dev/null; then
+  valgrind --quiet --leak-check=yes ./hash
+  if [ $? -ne 0 ]; then
+    echo '[FAIL]: Memory leak found'
+    exit 1
+  fi
+# check for memory leaks on macOS
+elif command -v leaks > /dev/null; then
+  tempfile=mktemp
+  (MallocStackLogging=1 leaks --quiet --atExit -- ./hash) >$tempfile 2>&1
+  if [ $? -ne 0 ]; then
+    cat $tempfile
+    rm $tempfile
+    echo '[FAIL]: Memory leak found'
+    exit 1
+  fi
+  rm $tempfile
+else
+  echo '[ERROR]: No memory checker found'
+  exit 1
+fi
 
 rm hash
